11e12f2089a13794d609ac425197ba967a87f62d
thaihoan2703 <hoanthai1103@gmail.com>
2023-08-03 22:49:13 +0700

    df

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const path = require('path');
-const { engine } = require('express-handlebars');
-const morgan = require('morgan');
+const express = require("express");
+const path = require("path");
+const { engine } = require("express-handlebars");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
-const route = require('./routes');
+const route = require("./routes");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //HTTP logger
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 //Template engine
-app.engine('hbs', engine({ extname: 'hbs' }));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources\\views'));
+app.engine("hbs", engine({ extname: "hbs" }));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources\\views"));
 
 // Routes
 
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
