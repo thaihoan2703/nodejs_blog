6f8a54e9f1c9cf20e276c6a8083ad76371c6c6e7
thaihoan2703 <hoanthai1103@gmail.com>
2023-08-16 22:21:38 +0700

    Create function sort column use middlewares

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -8,7 +8,15 @@ const {
 class MeController {
     //[GET] /me/stored/courses
     storedCourses(req, res, next) {
-        Promise.all([Course.find({}), Course.findDeleted({})])
+        let courseQuery = Course.find({});
+
+        if (req.query.hasOwnProperty("_sort")) {
+            courseQuery = courseQuery.sort({
+                [req.query.column]: req.query.type,
+            });
+        }
+
+        Promise.all([courseQuery, Course.findDeleted({})])
             .then(([courses, deletedCount]) => {
                 res.render("me/stored-courses", {
                     deletedCount: deletedCount.filter(
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const route = require("./routes");
 const db = require("./config/db");
 const app = express();
 const port = 3000;
+const SortMiddleware = require("./app/middlewares/SortMiddleware");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,10 +22,36 @@ app.use(express.static(path.join(__dirname, "public")));
 //HTTP logger
 app.use(morgan("combined"));
 
+//Custom middlewares
+app.use(SortMiddleware);
+
 //Template engine
 app.engine(
     "hbs",
-    engine({ extname: "hbs", helpers: { sum: (a, b) => a + b } })
+    engine({
+        extname: "hbs",
+        helpers: {
+            sum: (a, b) => a + b,
+            sortable: (field, sort) => {
+                const sortType = field === sort.column ? sort.type : "default";
+                const icons = {
+                    default: "oi oi-elevator",
+                    asc: "oi oi-sort-ascending",
+                    desc: "oi oi-sort-descending",
+                };
+                const types = {
+                    default: "desc",
+                    asc: "desc",
+                    desc: "asc",
+                };
+                const icon = icons[sortType];
+                const type = types[sortType];
+                return `<a href="?_sort&column=${field}&type=${type}">
+                            <span class="${icon}"></span>
+                        </a>`;
+            },
+        },
+    })
 );
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources", "views"));
